ed0f9d1e497448c826ce5547b0b0f8a2b786b67a
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-12 00:08:22 +0100

    :sparkles: Extracted Sanity config and added block content renderer

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,9 +1,14 @@
 import sanityClient from '@sanity/client'
 
-export default sanityClient({
-    // Find your project ID and dataset in `sanity.json` in your studio project
+export const config = {
     projectId: '4i02tf2x',
     dataset: 'production',
+}
+
+export default sanityClient({
+    // Find your project ID and dataset in `sanity.json` in your studio project
+    projectId: config.projectId,
+    dataset: config.dataset,
     useCdn: true,
     // useCdn == true gives fast, cheap responses using a globally distributed cache.
     // Set this to false if your application require the freshest possible
