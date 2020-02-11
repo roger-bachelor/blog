47a98be9195a38b607b28cc8071a8aa540505db3
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-11 22:21:19 +0100

    :construction: Updated posts page

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+// Getting da slugs
+import { getPostBySlug } from '../../data/post'
 
 import css from './Post.module.scss'
 
-export const PostPage: React.FC = () => <></>
+import { IPost } from '../../types/post'
+
+export const PostPage: React.FC<RouteComponentProps> = ({ match }) => {
+    const [post, setPost] = useState<IPost>()
+
+    useEffect(() => {
+        const { slug = '' } = match.params as any
+
+        getPostBySlug(slug)
+            .then(post => setPost(post))
+            .catch(err => console.log(err))
+    }, [match.params])
+
+    return (
+        <>
+            {post ? (
+                <article>
+                    <h1>{post.title}</h1>
+                    <p>{post.slug.current}</p>
+                </article>
+            ) : (
+                <p>OOPS</p>
+            )}
+        </>
+    )
+}
