403c91712e8de2cfbf861b168780b6b7a509a969
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-12 00:04:24 +0100

    :construction: Improved pages setup

--- src/pages/Author/Author.tsx
+++ src/pages/Author/Author.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
 
 import css from './Author.module.scss'
 
-export const AuthorPage = () => <></>
+export const AuthorPage: React.FC<RouteComponentProps> = () => (
+    <article className={css.container}>
+        <h1>Author</h1>
+    </article>
+)
