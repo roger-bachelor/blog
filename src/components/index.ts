b9b2baecc69128a6e3035ded979613d8e8215d17
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-12 00:08:22 +0100

    :sparkles: Extracted Sanity config and added block content renderer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Box } from './Box'
 export { BlogPostBox } from './BlogPostBox'
 export { BlogPostsContainer } from './BlogPostsContainer'
+export { BlockContent } from './BlockContent'
 export { Header } from './Header'
 export { Markdown } from './Markdown'
 export { SocialIcons } from './SocialIcons'
