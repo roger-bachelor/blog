2ccb29d98b117b17a8c496adc367ea0eae0a13ef
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-11 22:19:13 +0100

    :construction: Navbar stub

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '..'
+
+import css from './Navbar.module.scss'
+
+export const Navbar = () => (
+    <Box>
+        <nav className={css.container}></nav>
+    </Box>
+)
