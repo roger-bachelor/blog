4374d2d3ac98636b95d7ff1390d2068349ea34d8
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-13 00:00:46 +0100

    :art: Improved data layer

--- src/data/post.ts
+++ src/data/post.ts
@@ -4,9 +4,9 @@ import groq from 'groq'
 import { IPost } from '../types/post'
 
 export const getPostBySlug = async (slug: string): Promise<IPost> =>
-    (
-        await sanity.fetch(groq`*[_type == "post" && slug.current == "${slug}"]`)
-    )[0] as IPost
+    (await sanity.fetch(
+        groq`*[_type == "post" && slug.current == "${slug}"][0]`,
+    )) as IPost
 
 export const getAllPosts = async (): Promise<IPost[]> =>
     (await sanity.fetch(groq`*[_type == "post"]`)) as IPost[]
