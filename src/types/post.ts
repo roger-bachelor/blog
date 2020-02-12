884af3777d76886d564b2d0ada28a65135e64116
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-02-12 23:58:08 +0100

    🧢 Updated types

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,8 +1,9 @@
 import { IAuthor } from './author'
 import { ICategory } from './category'
 import { Slug } from './slug'
+import { SanityMeta } from './sanity-meta'
 
-export interface IPost {
+export interface IPost extends SanityMeta {
     title: string
     slug: Slug
 
---
+++ src/types/sanity-meta.ts
@@ -0,0 +1,7 @@
+export interface SanityMeta {
+    _createdAt: string
+    _id: string
+    _rev: string
+    _type: string
+    _updatedAt: string
+}
