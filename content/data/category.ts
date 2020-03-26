c872f1d559ce3d83f8a047561e7095ffdba9e8ca
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2020-03-26 06:08:24 +0100

    :art: Move data stuff

--- content/data/category.ts
+++ content/data/category.ts
@@ -3,8 +3,8 @@ import groq from 'groq'
 
 import { ICategory } from '../types/category'
 
-const getCategoryBySlug = async (): Promise<ICategory> =>
+export const getCategoryBySlug = async (): Promise<ICategory> =>
     (await sanity.fetch(groq`*`))[0] as ICategory
 
-const getAllCategories = async (): Promise<ICategory[]> =>
+export const getAllCategories = async (): Promise<ICategory[]> =>
     (await sanity.fetch(groq`*`)) as ICategory[]
